f07902dacdb6a22d709c1b353d6982403cb8c808
Juan Kaenel <kaenel10@gmail.com>
2021-03-02 21:32:11 -0300

    :tada: Iniciando plugin para mi sitio web

---
+++ personalPage_post_types.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+    Plugin Name: PersonalPageJK - Post Types
+    Plugin URI:
+    Description: Añade Post Types al sitio Juan Kaenel
+    Version: 1.0.0
+    Author: Juan Kaenel
+    Author URI: https://twitter.com/juankaenel1
+    Text Domain: PersonalPageJK
+*/
